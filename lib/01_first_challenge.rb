5b87823bafdd9fa01cd34504c4665c77c2f4c864
Susana vik <susana.m.vik@gmail.com>
2021-02-10 03:45:57 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,5 +1,15 @@
 def first_challenge
   epic_tragedy = {
-    :montague => {},
-    :capulet => {}
-  }
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
